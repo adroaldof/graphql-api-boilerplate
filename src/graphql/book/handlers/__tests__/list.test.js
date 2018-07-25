428ea0bb9f47831f8b4a68d0f2ef0727c2591a6a
Adroaldo de Andrade <adroaldof@gmail.com>
2018-07-25 12:32:07 -0300

    Remove old book handlers

--- src/graphql/book/handlers/__tests__/list.test.js
+++
@@ -1,8 +0,0 @@
-import handlers from '..';
-import books from '../fake-data';
-
-describe('book handler list', () => {
-  it('expect to list books', () => {
-    expect(handlers.list()).toEqual(books);
-  });
-});
--- src/graphql/book/handlers/list.js
+++
@@ -1,5 +0,0 @@
-import books from './fake-data';
-
-export default function list() {
-  return books;
-}
